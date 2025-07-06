0a48798546c2031d72ceff4de1991d219ebc8d0b
Sk Najir <sk.najir8392@gmail.com>
2025-07-06 13:51:50 +0530

    update generate-2

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -76,50 +76,7 @@ const Page = () => {
       setUrl("");
       setShortUrl("");
     }
-    // const myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json");
 
-    // const requestOptions = {
-    //   method: "POST",
-    //   headers: myHeaders,
-    //   body: raw,
-    //   redirect: "follow",
-    // };
-
-    // fetch(`${process.env.NEXT_PUBLIC_HOST}/api/generate`, requestOptions)
-    //   .then((response) => response.json())
-    //   .then((result) => {
-    //     // console.log(result);
-    //     // alert(result.message);
-    //     setGenerate(shortUrl);
-    //     setGenerating(0);
-    //     setUrl("");
-    //     setShortUrl("");
-    //     toast.success("Shorten url generated successfully", {
-    //       position: "top-right",
-    //       autoClose: 2500,
-    //       hideProgressBar: false,
-    //       closeOnClick: false,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //       transition: Bounce,
-    //     });
-    //   })
-    //   .catch((error) =>
-    //     toast.error("Internal Server Error", {
-    //       position: "top-right",
-    //       autoClose: 2500,
-    //       hideProgressBar: false,
-    //       closeOnClick: false,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //       progress: undefined,
-    //       theme: "light",
-    //       transition: Bounce,
-    //     })
-    //   );
   };
   const randomHandle = () => {
     const chars = "abcdefghijklmnopqrstuvwxyz";
